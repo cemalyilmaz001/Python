f1ddc67a00ababb9a1b4a5b3808fc8d945db4a68
Cemal YILMAZ <69977605+cemalyilmaz001@users.noreply.github.com>
2024-04-26 17:04:24 +0300

    .

---
+++ overloading/over.py
@@ -0,0 +1,17 @@
+# Overloading
+
+def add(a=0, b=0, c=0):
+	sayac = 0
+	if a >= 1:
+		sayac += 1
+
+	if b >= 1:
+		sayac += 1
+
+	if c >= 1:
+		sayac += 1
+
+	return ((a + b + c) / int(sayac))
+
+print(add(2, 3))     # Output: 5
+print(add(2, 3, 4))  # Output: 9
