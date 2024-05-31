9c846f0615be362bf6ecb412c2ae2023eda9f157
Cemal YILMAZ <69977605+cemalyilmaz001@users.noreply.github.com>
2024-05-31 04:49:01 -0400

    Update odev.py

--- Prog_Temel/odev.py
+++ Prog_Temel/odev.py
@@ -4,33 +4,33 @@
 #
 # Calisan
 # 
-class Calisan():
-    def __init__(self):
-        super().__init__()
-        self.adSoyad = "Cemal YILMAZ"
-        self.telefon = 5523213213
-        self.eposta  = "x@yaani.com" 
+class Calisan:
+    def __init__(self, adSoyad, telefon, eposta):
+        self.adSoyad = adSoyad
+        self.telefon = telefon
+        self.eposta = eposta
+
+    def set(self):
+        print(self.adSoyad)
+        print(self.telefon)
+        print(self.eposta)
 
     def giris(self):
         self.derseGir()
-    
+
     def cikis(self):
-        pass
-    
+        print("Çıkış Yapıldı")
+
     def yemekhane(self):
-        pass
+        print("Yemekhane")
 
 
-#
-# Akademisyen
-#
-class LabAsistani():
+class LabAsistani:
     def __init__(self):
-        super().__init__()
         pass
 
     def lablaraGir(self):
-        pass
+        print("Labarotuvara Giriş Yapıldı")
 
 
 class Asistan(LabAsistani):
@@ -42,17 +42,23 @@ class Asistan(LabAsistani):
         pass    
 
 class Akademisyen(Calisan):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, adSoyad, telefon, eposta):
+        super().__init__(adSoyad, telefon, eposta)
         self.bolum = "Bilişim Güvenliği"
-        self.ünvan = "SOC"
+        self.unvan = "SOC"
+
+    def akadmsySet(self):
+        print(self.adSoyad)
+        print(self.telefon)
+        print(self.eposta)
 
     def derseGir(self):
         print("hello World")
 
+
 class OgrtmGorevlisi(Akademisyen):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, adSoyad, telefon, eposta):
+        super().__init__(adSoyad, telefon, eposta)
         self.kapiNo = 33
 
     def senatoToplanti(self):
@@ -61,50 +67,46 @@ class OgrtmGorevlisi(Akademisyen):
     def sinavYap(self):
         pass
 
-
-
-#
-# Memur
-#
-
 class Memur(Calisan):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, adSoyad, telefon, eposta):
+        super().__init__(adSoyad, telefon, eposta)
         self.departman = "Bilgi İşlem"
-        self.mesai     = "8.5"
+        self.mesai = "8.5"
         
     def calis(self):
         print(self.adSoyad)
+        print(self.telefon)
+        print(self.eposta)
 
-        
-class Bilgiİslem(Memur):
-    def __init__(self):
-        super().__init__()
+class BilgiIslem(Memur):
+    def __init__(self, adSoyad, telefon, eposta):
+        super().__init__(adSoyad, telefon, eposta)
         self.gorev = ""
 
     def networkKurulum(self):
         pass
 
-
-class GuvenlkGorevlisi(Memur):
-    def __init__(self):
-        super().__init__()
+class GuvenlikGorevlisi(Memur):
+    def __init__(self, adSoyad, telefon, eposta):
+        super().__init__(adSoyad, telefon, eposta)
         self.belge = "xx11"
 
     def nobel(self):
         pass
 
-
-
-
 if __name__ == "__main__":
-    memur = Memur()
+    calis = Calisan("ASD SA", "034112333", "x@s.com")
+    calis.set()
+    print(30*"-")
+
+    memur = Memur("Ahmet Kaya", "034112334", "y@x.com")
     print(memur.departman)
     print(memur.mesai)
     memur.calis()
+    print(30*"-")
 
-    akadmsyn = Akademisyen()
+    akadmsyn = Akademisyen("Ayşe Demir", "034112335", "z@x.com")
     print(akadmsyn.bolum)
-    print(akadmsyn.ünvan)
-
-
+    print(akadmsyn.unvan)
+    akadmsyn.akadmsySet()
+    
